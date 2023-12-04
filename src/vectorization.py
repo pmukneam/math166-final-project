6cbbce876da49769552ce9b8c3f3434f6aca4e9a
GusAlbach <123139458+GusAlbach@users.noreply.github.com>
2023-12-03 22:22:44 -0800

    Update vectorization.py

--- src/vectorization.py
+++ src/vectorization.py
@@ -10,13 +10,13 @@ def vectorize_training_df(training_df):
     # vectorize the training data, using the tf-idf method.
     tfidf_vectorizer = TfidfVectorizer()
     tfidf_matrix = pd.DataFrame(tfidf_vectorizer.fit_transform(training_df['text']).toarray(), columns=tfidf_vectorizer.get_feature_names_out())
-    df = pd.concat([df, tfidf_matrix], axis=1)
+    df_train = pd.concat([df, tfidf_matrix], axis=1)
 
 def vectorize_leftover_df(leftover_df):
     # Not sure if this function is needed!
     # vectorize the leftover data, using the tf-idf method.
     tfidf_vectorizer = TfidfVectorizer()
     tfidf_matrix = pd.DataFrame(tfidf_vectorizer.fit_transform(leftover_df['text']).toarray(), columns=tfidf_vectorizer.get_feature_names_out())
-    df = pd.concat([df, tfidf_matrix], axis=1)
+    df_leftover = pd.concat([df, tfidf_matrix], axis=1)
 
-print(pd.head(df))
+print(pd.head(df_train))
